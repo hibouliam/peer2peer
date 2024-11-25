4889ca8ed71810dc3b9cbdce5a1daa5fefdd249e
hibouliam <huissoudlam@aol.com>
2024-11-25 23:12:14 +0100

    relation dht entre pairs + dht local

--- dht.py
+++ dht.py
@@ -1,8 +1,11 @@
+import msgpack
+import socket
+
 active_peers =[['5fe96ad748b11f2e00af23d0899e1bcff4655811831499732e5193937e3c8072fc9f44060acb7db8a3969c0a3df7fc5f52f0607d137bd684725081e6c3894f19', '127.0.0.1', 7001], ['b048dee8bf0ca95792006780bf7cff3a68cb4e37ff35b36313bd83576b02021ce7c0410b8a35613f6ae26507e788f7d4d6af389ec716a9c95729b486e063b20e', '127.0.0.1', 7004]]
 peer = ['6fe96ad748b11f2e00af23d0899e1bcff4655811831499732e5193937e3c8072fc9f44060acb7db8a3969c0a3df7fc5f52f0607d137bd684725081e6c3894f19', '127.0.0.1', 7002]
 def assign_dht(peer: list,active_peers:list) -> tuple :
     """
-    Asssigne la plage de la dht au pair en fonction de ses voisins
+    Asssigne la plage de la dht au pair en fonction de ses voisins en utilisant la fonction determine_responsibility
     """
     peer_key = int(peer [0],16)
     
@@ -25,5 +28,88 @@ def determine_responsibility(peer_key : int, left_neighbor_key : int, right_neig
     else :
         return(peer_key, max(left_neighbor_key,right_neighbor_key))
     
-print(assign_dht(peer, active_peers))
-    
+
+
+
+dht = {}  
+
+
+def add_file_to_dht_local(dht:dict, key:str, localisations:list)->  dict :
+    """
+    Ajoute un fichier à la DHT local.
+    """
+    if key in dht :
+        for peer in localisations :
+            if peer not in dht[key] :
+                dht[key].append(peer)
+        return dht
+    else :
+        dht[key]=localisations
+        return dht
+
+def send_file(message:bytes, key:str, active_peers: list, start:int, end : int) -> None:
+    """
+    Envoie le fichier a son plus proche voisin
+    """
+    try :
+        active_peers = sorted(active_peers,key=lambda peer: int(peer[0], 16)) 
+        if key > end :
+            ip = active_peers[2][1]
+            port = active_peers[2][2]
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+                client_socket.connect((ip, port))
+                client_socket.sendall(message)
+        if key < start :
+            ip = active_peers[1][1]
+            port = active_peers[1][2]
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+                client_socket.connect((ip, port))
+                client_socket.sendall(message)
+    except :
+        print("Problème lors de l'envoi du fichier")
+
+def send_dht_local(dht:dict,peer:list) -> None :
+    """
+    Envoie la dht local à un autre pair lors de la déconnexion
+    avec peer qui est la liste du pair a qui envoyer la dht
+    """
+    try:
+        message= {"action":"send_dht", "data": {"dht":dht}}
+        msgpack_dht = msgpack.packb(message)
+        ip = peer[1]
+        port = peer[2]
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+            client_socket.connect((ip, port))
+            client_socket.sendall(msgpack_dht)
+    except Exception as e:
+        print(f"Erreur lors de l'envoi de la DHT à {peer}: {e}")
+
+def merge_dht(dht_local:dict, dht_recu:dict) -> dict :
+    """
+    Fusionne la dht du peer qui se déconnecte avec la sienne
+    """
+    for key, localisations in dht_recu.items():
+        dht_local=add_file_to_dht_local(dht_local,key,localisations)
+    return dht_local
+
+def handle_dht(peer:list, active_peers: list, message:bytes,dht_local:dict) -> dict :
+    """
+    Gère les messages recu qui ont pour but de modifier a dht
+    """
+    try :
+        received_data = msgpack.unpackb(message)
+        action = received_data.get("action")
+        data = received_data.get("data")
+        if action == "add_file":
+            key=int(data.get("key"),16)
+            start,end=assign_dht(peer, active_peers)
+            if (key>start and key<end) or (end is None and key>start):
+                localisations = data.get("localisations")
+                return add_file_to_dht_local(dht_local, key, localisations)
+            else :
+                return send_file(message, key,active_peers, start, end)
+        if action == "send_dht":
+            dht_recu=data.get("dht", {})
+            return merge_dht(dht_local, dht_recu)
+    except :
+        print("problème avec la handle_dht")
